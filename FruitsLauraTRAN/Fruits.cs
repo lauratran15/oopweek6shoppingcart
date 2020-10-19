5c8077c013e1d6091f6bef55f8dbf1478c88650e
laura <laura.tran@edu.devinci.fr>
2020-10-19 15:48:24 +0100

    Ajoutez des fichiers projet.

---
+++ FruitsLauraTRAN/Fruits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FruitsLauraTRAN
+{
+    public class Fruits
+    {
+        private string NameFruit { get; set; }
+        public double Price { get; set; }
+
+        public Fruits(string name)
+        {
+            NameFruit = name;
+            if (name == "apple")
+            {
+                Price = 0.60;
+            }
+            else
+            {
+                if(name == "orange")
+                {
+                    Price = 0.25;
+                }
+                    
+            }
+        }
+
+       
+    }
+}
---
+++ FruitsLauraTRAN/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FruitsLauraTRAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fruits> listFruit = new List<Fruits>();
+            double cost = 0;
+
+            Fruits fruitA = new Fruits("appleA");
+            listFruit.Add(fruitA);
+            Fruits fruitB = new Fruits("orangeA");
+            listFruit.Add(fruitB);
+            Fruits fruitC = new Fruits("appleB");
+            listFruit.Add(fruitC);
+            Fruits fruitD = new Fruits("orangeB");
+            listFruit.Add(fruitD);
+
+            foreach (Fruits fruit in listFruit)
+            {
+                cost = cost + fruit.Price;
+            }
+            Console.WriteLine(cost);
+            Console.ReadKey();
+        }
+    }
+}
